dea6128a593d66206b36beb4f257cb0deb3a4d79
olga-passos39 <olgapassos457@gmail.com>
2024-09-18 15:47:10 -0300

    first commit

---
+++ ativ20.py
@@ -0,0 +1,8 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+
+numero = 0
+
+while numero <= 100:
+    if numero % 2 == 0:
+        print(numero)
+    numero += 1
